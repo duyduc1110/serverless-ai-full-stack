c93228a2662b886ff247dd10c1da1f3fe83be9b1
duyduc1110 <duyduc1110@gmail.com>
2023-06-03 14:43:20 +0700

    add label for user and images

--- ads-generator/static/script.js
+++ ads-generator/static/script.js
@@ -1,46 +1,81 @@
 $(document).ready(function() {
-  
-   $('#next-to-audience').prop('disabled', true);
-  
-   $('#brand-page form').on('input', function(){
-      if($('#brand').val()){
-         $('#next-to-audience').prop('disabled', false);
-      }else{
-         $('#next-to-audience').prop('disabled', true);
-      }
-   });
-  
-   $('#add-audience').click(function(e) {
-      e.preventDefault();
-      const audienceGroup = $('<div class="audience-group"><input class="audience" type="text" name="audience[]"><button class="remove-audience" type="button"><i class="fa fa-trash"></i></button><br></div>');
-      audienceGroup.find('.remove-audience').click(function(e) {
-         e.preventDefault();
-         $(this).closest('.audience-group').remove();
-      });
-      $('#audience-container').append(audienceGroup);
-   });
-   
-   $('.remove-audience').click(function(e) {
-      e.preventDefault();
-      $(this).closest('.audience-group').remove();
-   });
-   
-   $('#next-to-audience').click(function(e){
-     e.preventDefault();
+   let audienceCount = 1;
+ 
+   $('#next-to-audience').click(function() {
      $('#brand-page').hide();
      $('#audience-page').show();
    });
-   
-   $('#next-to-image').click(function(e){
-     e.preventDefault();
-     const brand = $('#brand').val();
-     const audience = $('.audience').map(function() { return $(this).val(); }).get();
-     
-     $.post('/generate', {brand, audience}, function(data){
-       const images = data.images.map(image => `<img src="data:image/jpeg;base64,${image}"/>`).join('');
-       $('#image-container').html(images);
-       $('#audience-page').hide();
-       $('#image-page').show();
+ 
+   $('#next-to-image').click(function() {
+     $('#audience-page').hide();
+     $('#image-page').show();
+     // get the brand and audience descriptions from the input fields
+     let brand = $('#brand').val();
+     let audience = $('.audience').map(function() {
+       return $(this).val();
+     }).get();
+     // send a POST request to the /generate route with the form data
+     $.ajax({
+       url: '/generate',
+       type: 'POST',
+       data: {
+         brand: brand,
+         audience: audience
+       },
+       success: function(data) {
+         // loop through the image data and create image elements
+         for (let i = 0; i < data.images.length; i++) {
+           // create a label element with the audience description
+           let label = $('<label/>', {
+             'text': 'User ' + (i + 1) + ': ' + audience[i]
+           });
+           // create an image element with the base64 encoded image data
+           let image = $('<img/>', {
+             'src': 'data:image/png;base64,' + data.images[i],
+             'alt': 'Image for user ' + (i + 1)
+           });
+           // append the label and image elements to the #image-container div
+           $('#image-container').append(label).append(image).append('<br>');
+         }
+       },
+       error: function(error) {
+         // handle the error
+         alert(error.responseJSON.error);
+       }
      });
    });
-});
+ 
+   $('#add-audience').click(function() {
+     audienceCount++;
+     let audienceGroup = $('<div/>', {
+       'class': 'audience-group'
+     });
+     let label = $('<label/>', {
+       'text': 'User ' + audienceCount + ':'
+     });
+     let input = $('<input/>', {
+       'class': 'audience',
+       'type': 'text',
+       'name': 'audience[]',
+       'placeholder': 'input text'
+     });
+     let removeButton = $('<button/>', {
+       'class': 'remove-audience',
+       'type': 'button'
+     });
+     let icon = $('<i/>', {
+       'class': 'fa fa-trash'
+     });
+     removeButton.append(icon);
+     audienceGroup.append(label).append('<br>').append(input).append(removeButton).append('<br>');
+     $('#audience-container').append(audienceGroup);
+   });
+ 
+   $(document).on('click', '.remove-audience', function() {
+     $(this).closest('.audience-group').remove();
+     audienceCount--;
+     $('.audience-group label').each(function(index) {
+       $(this).text('User ' + (index + 1) + ':');
+     });
+   });
+ });
